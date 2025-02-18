0ed1c170ce64ba8408521ee1786402ab0d2ece23
wimansi-navoda <153409947+wimansi-navoda@users.noreply.github.com>
2025-02-18 15:20:58 +0530

    Initial commit

---
+++ test_cases/test_case1.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys  
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+service = Service(r'C:\selenium_testing\chromedriver.exe')  
+
+
+driver = webdriver.Chrome(service=service)
+
+
+driver.get("https://www.python.org")
+
+
+print(driver.title)
+
+
+search_bar = driver.find_element(By.NAME, "q")
+
+search_bar.clear()
+search_bar.send_keys("getting started with python")
+search_bar.send_keys(Keys.RETURN) 
+
+
+print(driver.current_url)
+
+input (10)
+
+
+driver.quit()
